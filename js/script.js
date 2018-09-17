54d1692e936a323c668439256e9570af09e53a99
Andrey Moshkov <fakesupp@gmail.com>
2018-09-17 09:28:56 +0300

    first-commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+$(document).ready(function() {
+  $('.teachers__list').slick({
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 5000,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      }
+    ]
+  });
+});
